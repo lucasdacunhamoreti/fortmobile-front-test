80505e7947f644a994464162a06bbbf30d976545
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2023-02-07 11:49:52 -0300

    feat: Creates user registration and listing modules

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full' ,redirectTo: 'register' },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
